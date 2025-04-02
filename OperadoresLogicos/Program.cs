21453df1cb5084801b9228d55e583a11826647ed
Paulo Cesar <pauloandradeoliveira2004@gmail.com>
2025-04-02 11:28:53 -0300

    Estudo Operador logico

---
+++ OperadoresLogicos/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("\nOperadores Logicos");
+
+/*
+ 
+  && E (and)
+    O operador Lógico (&&) retorna (True) se (ambas) as expressões forem (verdadeiras).
+    
+  || --> Ou (or)
+    O operador lógico (||) retorna (True) se pelo menos uma das expressões for (verdadeiros).
+    
+  ! --> Navegação (not)
+    O operador lógico (!) inverte o valor de uma expressão booleana.
+  
+ */
+
+
+int numero1 = 1;
+int numero2 = 2;
+
+// Operador Lógico ( && )
+bool valido = numero1 < 8 && numero1 < 9;
+Console.WriteLine(valido);
+
+// Operador Lógico 
+bool valido2 = numero1 > 10 || numero2 < 8;
+Console.WriteLine(valido2);
--- OperadoresLogicos/obj/Debug/net9.0/OperadoresLogicos.AssemblyInfo.cs
+++ OperadoresLogicos/obj/Debug/net9.0/OperadoresLogicos.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("OperadoresRelacionais")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("OperadoresLogicos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed653b21febfdb436024dd30a83180382b96c8cf")]
-[assembly: System.Reflection.AssemblyProductAttribute("OperadoresRelacionais")]
-[assembly: System.Reflection.AssemblyTitleAttribute("OperadoresRelacionais")]
+[assembly: System.Reflection.AssemblyProductAttribute("OperadoresLogicos")]
+[assembly: System.Reflection.AssemblyTitleAttribute("OperadoresLogicos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
